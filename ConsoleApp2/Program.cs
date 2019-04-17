9b45e23524349f65b502ff77a409648c01047d4f
visualstudio <visualstudio@122-PC24>
2019-04-17 12:19:51 -0400

    Sukhdeep1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,15 +1,85 @@
 ﻿using System;
+
 using System.Collections.Generic;
+
 using System.Linq;
+
 using System.Text;
+
 using System.Threading.Tasks;
 
-namespace ConsoleApp2
+
+
+namespace LongestWord
+
 {
+
     class Program
+
     {
+
+        string a;
+
         static void Main(string[] args)
+
         {
+
+            Program a = new Program();
+
+            Console.WriteLine("The longest word is: ");
+
+            a.MyFunctionA("Ada Lovelace wrote the first algorithm designed for procession by an Analytical Engine");
+
         }
+
+
+
+        public void MyFunctionA(string output)
+
+        {
+
+            string[] words = output.Split(' ');
+
+            string b;
+
+
+
+            int wordArrayLength = words.Length;
+
+            int x = 0;
+
+            foreach (var word in words)
+
+            {
+
+
+
+                foreach (var word2 in words)
+
+                {
+
+                    if (word2.Length > word.Length)
+
+                    {
+
+                        a = word2;
+
+
+
+                    }
+
+                }
+
+            }
+
+            Console.WriteLine("Longest word is " + a);
+
+            Console.ReadLine();
+
+
+
+        }
+
     }
+
 }
